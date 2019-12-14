d9e3f8473c701137be8e73343712e949182ca816
FrancisVijith <55200827+FrancisVijith@users.noreply.github.com>
2019-12-14 17:02:42 -0500

    Update ball_dsply_mvmnt.c

--- inc/ball_dsply_mvmnt.c
+++ inc/ball_dsply_mvmnt.c
@@ -11,43 +11,100 @@ SW = southwest and SE = southeast
 
 #ifndef BALL_DSPLY_MVMNT_C_
 #define BALL_DSPLY_MVMNT_C_
-void display_ball()
+
+#include "game_objects.h"
+
+unsigned int flag = 0;
+unsigned int display;
+void display_Ball()
 {
 
 }
-void check_Coll_Top();
-void check_Coll_Right();
-void check_Coll_Btm();
-void check_Coll_Left();
 
 void  Ball_NW()
 {
 	display_Ball();
 	check_Coll_Top();
 	check_Coll_Left();
+	if (flag == 2)
+    {pos_ball.Xnew = pos_ball.X-1;
+	pos_ball.Ynew = pos_ball.Y -1;
+	pos_ball.x = pos_ball.Xnew;
+	pos_ball.y = pos_ball.Ynew;
+    }
+	else
+	break;
 	display_Ball();
 }
-void  Ball_NW()
+void  Ball_NE()
 {
 	display_Ball();
 	check_Coll_Top();
 	check_Coll_Right();
+	if (flag == 2)
+	    {pos_ball.Xnew = pos_ball.X + 1;
+		pos_ball.Ynew = pos_ball.Y - 1;
+		pos_ball.x = pos_ball.Xnew;
+		pos_ball.y = pos_ball.Ynew;
+	    }
+		else
+		break;
 	display_Ball();
 }
-void  Ball_NW()
+void  Ball_SW()
 {
 	display_Ball();
 	check_Coll_Btm();
 	check_Coll_Left();
+	if (flag == 2)
+	    {pos_ball.Xnew = pos_ball.X-1;
+		pos_ball.Ynew = pos_ball.Y +1;
+		pos_ball.x = pos_ball.Xnew;
+		pos_ball.y = pos_ball.Ynew;
+	    }
+		else
+		break;
 	display_Ball();
 }
-void  Ball_NW()
+void  Ball_SE()
 {
 	display_Ball();
 	check_Coll_Btm();
 	check_Coll_Right();
+	if (flag == 2)
+	    {pos_ball.Xnew = pos_ball.X+1;
+		pos_ball.Ynew = pos_ball.Y+1;
+		pos_ball.x = pos_ball.Xnew;
+		pos_ball.y = pos_ball.Ynew;
+	    }
+		else
+		break;
 	display_Ball();
 }
+
+
+void check_Coll_Top()
+{
+	if((pos_ball.X && pos_ball.Y) == ((pos_blocks.X - 5)&& (pos_ball.Y - 5))
+       ++flag;
+}
+void check_Coll_Right()
+{	if((pos_ball.X && pos_ball.Y) == ((pos_blocks.X - 5)&& (pos_ball.Y - 5))
+       Ball_SW();
+++flag;
+}
+void check_Coll_Btm()
+{
+	if((pos_ball.X && pos_ball.Y) == ((pos_blocks.X - 5)&& (pos_ball.Y - 5))
+       ++flag;
+};
+void check_Coll_Left()
+{
+	if((pos_ball.X && pos_ball.Y) == ((pos_blocks.X - 5)&& (pos_ball.Y - 5))
+       ++flag;
+}
+
+
 
 
 #endif /* BALL_DSPLY_MVMNT_C_ */
