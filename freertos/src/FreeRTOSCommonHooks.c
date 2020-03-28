41e04e581302baa1256f9d1648e682791c3477c3
FrancisVijith <55200827+FrancisVijith@users.noreply.github.com>
2020-03-28 18:02:57 -0400

    Update FreeRTOSCommonHooks.c

--- freertos/src/FreeRTOSCommonHooks.c
+++ freertos/src/FreeRTOSCommonHooks.c
@@ -27,7 +27,7 @@
  * is used in conjunction with NXP Semiconductors microcontrollers.  This
  * copyright, permission, and disclaimer notice must appear in all copies of
  * this code.
- */
+ 
 
 #include "FreeRTOS.h"
 #include "task.h"
